2bb12e9ce7125a01c68616782a7b7e215dd8fcdc
JoseCevallos17 <jose.cevallos.proano@udla.edu.ec>
2026-05-25 16:53:16 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,9 @@
+int menu();
+void leerCadena(char *cadena, int n);
+int validarEnteroConRango(int a, int b);
+float validarFloatConRango(int a, int b);
+int registrarProducto(char nombres[10][20], float *precio, int *stock, int cont);
+void verInventario(char nombres[10][20], float *precio, int *stock, int cont);
+void encontrarMasCaro(char nombres[10][20], float *precio, int cont);
+void encontrarMasBarato(char nombres[10][20], float *precio, int cont);
+void buscarProducto(char nombres[10][20], float *precio, int cont);
